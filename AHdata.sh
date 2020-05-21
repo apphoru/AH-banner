9f576109d3c59d87b29c181ae195028799c575a7
apphoru <63139427+apphoru@users.noreply.github.com>
2020-05-21 23:15:42 +0530

    Updated

--- AHdata.sh
+++ AHdata.sh
@@ -6,5 +6,5 @@ figlet Successfuly |lolcat
 figlet   Removed |lolcat
 echo -e "\033[1;96m"
 echo -e "Thanks For using Our Program"
-termux-open-url https://www.youtube.com/channel/UCaZUWHSho8-s3GECYrhB>
+termux-open-url https://www.youtube.com/channel/UCaZUWHSho8-s3GECYrhBwqg
 exit
